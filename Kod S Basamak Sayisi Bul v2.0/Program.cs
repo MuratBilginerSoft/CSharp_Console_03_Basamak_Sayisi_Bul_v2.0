6764e88cfb2c3923fe3da9a111e170d80e95ffd9
Murat Bilginer <muratbilginer09@gmail.com>
2019-12-18 03:52:47 +0300

    Initial commit

---
+++ Kod S Basamak Sayisi Bul v2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Basamak_Sayisi_Bul_v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sayi;
+
+            Console.Write("Sayıyı Giriniz:");
+
+            Sayi = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Basamak Sayısı={0}", Sayi.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
